5d244994cc684a1eda1294bce6fc530f49b66ba9
Sayed Jawid <benjimortal@gmail.com>
2024-11-21 09:00:55 +0100

    First commit - commeting all files to the repository

---
+++ crud_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import User, Product
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name','description','price']
